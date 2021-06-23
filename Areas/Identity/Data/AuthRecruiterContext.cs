9054005d34c0cda2decc4db25226cd5703ae3c5d
Kabza Ngwana <k.cheese.b@gmail.com>
2021-06-23 22:02:12 +0200

    Done with authentication

---
+++ Areas/Identity/Data/AuthRecruiterContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Recruiter.Areas.Identity.Data;
+
+namespace Recruiter.Data
+{
+    public class AuthRecruiterContext : IdentityDbContext<RecruiterUser>
+    {
+        public AuthRecruiterContext(DbContextOptions<AuthRecruiterContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Recruiter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
