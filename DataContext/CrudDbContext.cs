99da1b207ea4e92516e98b6974c8b42d8770aa6c
Kabza Ngwana <k.cheese.b@gmail.com>
2021-07-13 20:57:11 +0200

    Done with Models

---
+++ DataContext/CrudDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recruiter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiter.DataContext
+{
+    public class CrudDbContext : DbContext
+    {
+        public CrudDbContext(DbContextOptions<CrudDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Package> Packages { get; set; }
+    }
+}
---
+++ Models/Package.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiter.Models
+{
+    public class Package
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "In Stock")]
+        public bool InStock { get; set; }
+        [Display(Name = "Date Received")]
+        public DateTime DateStored { get; set; }
+    }
+}
